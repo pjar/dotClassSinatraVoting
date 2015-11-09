d65587f70b6d6e4cd28f12b9950e6b15b6f56092
Jarek Plonski <jarek.github@gmail.com>
2015-11-09 17:57:18 +0100

    demo

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require 'sinatra'
 
+Choices = {
+  'HAM' => 'Hamburger',
+  'PIZ' => 'Pizza',
+  'CUR' => 'Curry',
+  'NOO' => 'Noodles',
+}
+
 get '/' do
-  "Hello World"
+  erb :index
 end
 
+post '/cast' do
+  @vote = Choices[params[:vote]]
+  erb :result
+end
